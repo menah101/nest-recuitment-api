4e4dc6583ca7a4a0e033bc97dce7ef6d8f87f88d
quy_le_van <quy_le_van@bita.jp>
2024-08-05 08:47:13 +0700

    feat: config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://quy0694:7VNOqZ10bq4FN3Hc@recuitment.xbjnnmq.mongodb.net/',
-    ),
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://quy0694:7VNOqZ10bq4FN3Hc@recuitment.xbjnnmq.mongodb.net/',
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
