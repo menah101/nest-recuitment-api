bb689cbcc642fd8909a7e2c84bab159ddf573a82
quy_le_van <quy_le_van@bita.jp>
2024-08-05 10:45:03 +0700

    feat: DTO user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,12 +17,14 @@ export class UsersService {
     return hash;
   };
 
-  async create(email: string, password: string, name: string) {
-    const hashPassword = this.getHashPassword(password);
+  async create(createUserDto: CreateUserDto) {
+    const hashPassword = this.getHashPassword(createUserDto.password);
+
     const user = await this.userModel.create({
-      email,
+      email: createUserDto.email,
       password: hashPassword,
-      name,
+      name: createUserDto.name,
+      address: createUserDto.address,
     });
     return user;
   }
