548f6a8512fb32e99b701a482243e50befeed6d2
quy_le_van <quy_le_van@bita.jp>
2024-08-09 12:00:52 +0700

    feat: setup cors

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,19 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+
+  //config cors
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   await app.listen(configService.get<string>('PORT'));
 }
