cdb0ca8a28fd44480eee88089daa8b148c486bc5
quy_le_van <quy_le_van@bita.jp>
2024-08-12 14:24:48 +0700

    feat: update schema user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,23 +1,104 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
 
 export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'Name is not empty',
+  })
+  name: string;
+
   @IsEmail(
     {},
     {
-      message: 'Email không đúng đinh dạng',
+      message: 'Email not is valid',
     },
   )
   @IsNotEmpty({
-    message: 'Email không được để trống',
+    message: 'Email is not empty',
   })
   email: string;
 
   @IsNotEmpty({
-    message: 'Password không được để trống',
+    message: 'Password is not empty',
   })
   password: string;
 
+  @IsNotEmpty({
+    message: 'Age is not empty',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Gender is not empty',
+  })
+  gender: string;
+
+  @IsNotEmpty({
+    message: 'Address is not empty',
+  })
+  address: string;
+
+  @IsNotEmpty({
+    message: 'Role is not empty',
+  })
+  role: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({
+    message: 'Name is not empty',
+  })
   name: string;
 
+  @IsEmail(
+    {},
+    {
+      message: 'Email not is valid',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email is not empty',
+  })
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Password is not empty',
+  })
+  password: string;
+
+  @IsNotEmpty({
+    message: 'Age is not empty',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Gender is not empty',
+  })
+  gender: string;
+
+  @IsNotEmpty({
+    message: 'Address is not empty',
+  })
   address: string;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -18,10 +18,34 @@ export class User {
   age: number;
 
   @Prop()
-  address: string;
+  gender: string;
 
   @Prop()
-  phone: string;
+  address: string;
+
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
 
   @Prop()
   createdAt: string;
